996a368cf3ca5d88b4dfc3efcf35f103f8617fc7
Amar Maruf <amarmaruf2403@gmail.com>
2022-03-30 01:09:30 +0700

    menambahkan animasi pada home  dan menambahkan interaksi pada tombol button

--- script.js
+++ script.js
@@ -16,5 +16,28 @@ window.addEventListener("scroll", function () {
 });
 
 // animation web
-var Hero = document.getElementsByClassName("hero")[0];
-function loaded() {}
+function scrolled() {
+  var services = document.getElementById("services");
+  var projects = document.getElementsByClassName("fadein");
+  var elementVisible = 300;
+  var elementVisible2 = 10;
+  var windowHeight = window.innerHeight;
+  var elementTop = services.getBoundingClientRect().top;
+
+  if (elementTop < windowHeight - elementVisible) {
+    services.classList.add("active");
+  } else {
+    services.classList.remove("active");
+  }
+
+  for (let i = 0; i < projects.length; i++) {
+    var elemenTopProject = projects[i].getBoundingClientRect().top;
+    if (elemenTopProject < windowHeight - elementVisible2) {
+      projects[i].classList.add("active");
+    } else {
+      projects[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrolled);
